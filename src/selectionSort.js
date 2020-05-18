922c06bb0ead84251c3cb4269d725fe800694629
Deijah Wilson <deijahwilson1@gmail.com>
2020-05-18 11:03:27 -0400

    Done.

--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,22 @@
+
+
 function selectionSort(arr) {
-  // your code here!
-  // arr is an array of unsorted integers (i.e. [3, 5, 1])
+//for every item in the collection
+//find the smallest item in the unsorted portion of the array and swap it with the current item
+// arr is an array of unsorted integers (i.e. [3, 5, 1])
+function selectionSort(arr){
+  let num = 0
+  for(var i = 0;i < arr.length; i++){
+     let n = i + 1
+    while(n < arr.length){
+      num = arr[i]
+      num1 = arr[n]
+      if(arr[i] > arr[n]){
+        arr[n] = num
+        arr[i] = num1
+      }
+      n++
+    }
+  }
+  console.log(arr)
 }
